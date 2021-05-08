a2f2ce793bbbcd338e556874dbb674ac00b11cb4
nanunzoey <nanunzoey@gmail.com>
2021-05-08 15:10:55 +0900

    Update indeed.py

--- indeed.py
+++ indeed.py
@@ -1,12 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 
-LIMIT = 50
-URL = f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and=python&l=%EC%84%9C%EC%9A%B8&limit={LIMIT}"
 
-
-def extract_pn():
-    result = requests.get(URL)
+def extract_pn(url):
+    result = requests.get(url)
     soup = BeautifulSoup(result.text, 'html.parser')
 
     pagination = soup.find('div', {'class': 'pagination'})
@@ -21,18 +18,21 @@ def extract_pn():
 
 def extract_job_info(job_row):
     title = job_row.find('h2').find('a')['title']
-    company = job_row.find('div', {'class': 'sjcl'}).find(
-        'span', {'class': 'company'}).get_text(strip=True)
+    try:
+        company = job_row.find('div', {'class': 'sjcl'}).find(
+            'span', {'class': 'company'}).get_text(strip=True)
+    except:
+        company = None
     location = job_row.find('div', {'class': 'recJobLoc'})['data-rc-loc']
     link = 'https://kr.indeed.com'+job_row.find('h2').find('a')['href']
     return {'title': title, 'company': company, 'location': location, 'link': link}
 
 
-def extract_jobs(last_page):
+def extract_jobs(last_page, url):
     jobs = []
     for page in range(last_page):
         print(f"Scrapping Indeed Page: {page+1}")
-        result = requests.get(f"{URL}&start={page*LIMIT}")
+        result = requests.get(f"{url}&start={page*50}")
         soup = BeautifulSoup(result.text, 'html.parser')
         job_data = soup('div', {'class': 'jobsearch-SerpJobCard'})
 
@@ -42,8 +42,9 @@ def extract_jobs(last_page):
     return jobs
 
 
-def get_indeed_jobs():
-    last_page = extract_pn()
-    jobs = extract_jobs(last_page)
+def get_indeed_jobs(word):
+    url = f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and={word}&l=%EC%84%9C%EC%9A%B8&limit=50"
+    last_page = extract_pn(url)
+    jobs = extract_jobs(last_page, url)
     print(f"Total: {len(jobs)} jobs")
     return jobs
