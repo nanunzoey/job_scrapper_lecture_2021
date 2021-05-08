b36a5aa51b87e0a7ad3bb5f9b1c9072841694789
nanunzoey <nanunzoey@gmail.com>
2021-05-08 15:11:06 +0900

    Update stackoverflow.py

--- stackoverflow.py
+++ stackoverflow.py
@@ -1,11 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 
-URL = "https://stackoverflow.com/jobs?q=python"
 
-
-def extract_pn():
-    result = requests.get(URL)
+def extract_pn(url):
+    result = requests.get(url)
     soup = BeautifulSoup(result.text, 'html.parser')
 
     pagination = soup.find('div', {'class': 's-pagination'})
@@ -20,18 +18,18 @@ def extract_pn():
 def extract_job_info(job_row):
     title = job_row.find('h2', {'class': 'mb4'}).find('a')['title']
     company = job_row.find('h3').find('span').get_text(strip=True)
-    location = company = job_row.find('h3').find('span', {'class':
-                                                          'fc-black-500'}).get_text(strip=True)
+    location = job_row.find('h3').find('span', {'class':
+                                                'fc-black-500'}).get_text(strip=True)
     link = 'https://stackoverflow.com' + \
         job_row.find('h2', {'class': 'mb4'}).find('a')['href']
     return {'title': title, 'company': company, 'location': location, 'link': link}
 
 
-def extract_jobs(last_page):
+def extract_jobs(last_page, url):
     jobs = []
     for page in range(last_page):
         print(f"Scrapping StackOverflow Page: {page+1}")
-        result = requests.get(f"{URL}&pg={page+1}")
+        result = requests.get(f"{url}&pg={page+1}")
         soup = BeautifulSoup(result.text, 'html.parser')
         job_data = soup('div', {'class': 'grid--cell fl1'})
         for job_row in job_data:
@@ -40,8 +38,9 @@ def extract_jobs(last_page):
     return jobs
 
 
-def get_so_jobs():
-    last_page = extract_pn()
-    jobs = extract_jobs(last_page)
+def get_so_jobs(word):
+    url = f"https://stackoverflow.com/jobs?q={word}"
+    last_page = extract_pn(url)
+    jobs = extract_jobs(last_page, url)
     print(f"Total: {len(jobs)} jobs")
     return jobs
