51323751e455f8e0c7e408f5ad9069b305338bcc
nanunzoey <nanunzoey@gmail.com>
2021-05-08 15:10:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, send_file
+from stackoverflow import get_so_jobs
+from indeed import get_indeed_jobs
+from save import save_to_file
 
 app = Flask("JobScrapper")
+
+db = {}
 
 
 @app.route("/")
@@ -10,12 +15,34 @@ def home():
 
 @app.route("/report")
 def report():
-    searchingword = request.args.get('searchingword')
-    if searchingword:
-        searchingword = searchingword.lower()
+    word = request.args.get('word')
+    if word:
+        word = word.lower()
+        existing_jobs = db.get(word)
+        if existing_jobs:
+            jobs = existing_jobs
+        else:
+            jobs = get_indeed_jobs(word) + get_so_jobs(word)
+            db[word] = jobs
     else:
         return redirect("/")
-    return render_template("report.html", word=searchingword)
+    return render_template("report.html", searchingBy=word, resultNumber=len(jobs), jobs=jobs)
 
 
-app.run(host="0.0.0.0")
+@app.route("/export")
+def export():
+    try:
+        word = request.args.get('word')
+        if not word:
+            raise Exception()
+        word = word.lower()
+        jobs = db.get(word)
+        if not jobs:
+            raise Exception()
+        save_to_file(jobs)
+        return send_file('jobs.csv', mimetype='text/csv', attachment_filename='jobs.csv', as_attachment=True)
+    except:
+        return redirect("/")
+
+
+# app.run(host="0.0.0.0")
